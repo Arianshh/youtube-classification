ac5b42d916e608bb5cc7b34ba6f9b3fd4bad6064
Arian <arian.shariat@gmail.com>
2019-08-18 18:27:12 +0430

    Train network

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # youtube videos classification
 A neural network which classifies YouTube videos based on their category.
-Network is trained with top trending YouTube videos in Canada.
+Network is trained with top trending YouTube videos in Canada, USA and Great Britain.
 
 https://www.kaggle.com/datasnaek/youtube-new/
--- input_generator.py
+++ input_generator.py
@@ -6,7 +6,7 @@ def load_dataset_with_lables(dataframe, target_column):
     dataframe = dataframe.copy()
 
     target_attribute = dataframe.pop(target_column)
-    train_ds, test_ds, train_lb, test_lb = train_test_split(dataframe, target_attribute, test_size=0.2,random_state=1)
-    train_ds, val_ds, train_lb, val_lb = train_test_split(train_ds, train_lb, test_size=0.25, random_state=1)
+    train_ds, test_ds, train_lb, test_lb = train_test_split(dataframe, target_attribute, test_size=0.2)
+    train_ds, val_ds, train_lb, val_lb = train_test_split(train_ds, train_lb, test_size=0.25)
 
     return (train_ds, train_lb), (val_ds, val_lb), (test_ds, test_lb)
