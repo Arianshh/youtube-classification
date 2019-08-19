f0100b49640c93903efc7ffccbc5e028c093adfa
Arian <arian.shariat@gmail.com>
2019-08-19 09:35:51 +0430

    Add cvs_reader descriptions

--- cvs_reader.py
+++ cvs_reader.py
@@ -4,11 +4,17 @@ import pandas as pd
 
 
 def get_cvs_data(path):
+    """ reads csv file from path
+     and creates a pandas dataframe."""
+
     dataframe = pd.read_csv(path, error_bad_lines=False)
     return dataframe
 
 
 def load_dataframe(csvpath, columns):
+    """ Creates Dataframe with columns from
+     values of given columns list and normalizes tags column."""
+
     csvdf = get_cvs_data(csvpath)
     df = pd.DataFrame(csvdf, columns=columns)
     for i, col in enumerate(df.columns):
